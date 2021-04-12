7d05e94d4adad37389b33545b5afc2cd5d75933c
Adv35 <78689485+Adv35@users.noreply.github.com>
2021-04-12 16:00:11 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+let Biggestnumber = 0
+let numbervalues = [7, 23, 42, 36, 50, 20]
+for (let index = 0; index <= numbervalues.length; index++) {
+    if (numbervalues[index] > numbervalues[index + 1]) {
+        Biggestnumber = numbervalues[index]
+    } else if (numbervalues[index] < numbervalues[index + 1]) {
+        Biggestnumber = numbervalues[index + 1]
+    }
+}
+serial.writeValue("Biggest Number", Biggestnumber)
+basic.forever(function () {
+	
+})
