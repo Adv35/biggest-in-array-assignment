1a9c4621846cf632918bb42a0246a5de6d115af9
Adv35 <78689485+Adv35@users.noreply.github.com>
2021-05-20 18:58:53 +0200

    Did some small changes for no lagg

--- main.ts
+++ main.ts
@@ -1,13 +1,13 @@
 let Biggestnumber = 0
-let numbervalues = [7, 23, 42, 36, 50, 20]
+let numbervalues = [7, 23, 42, 43, 1, 20]
 for (let index = 0; index <= numbervalues.length; index++) {
     if (numbervalues[index] > numbervalues[index + 1]) {
         Biggestnumber = numbervalues[index]
     } else if (numbervalues[index] < numbervalues[index + 1]) {
         Biggestnumber = numbervalues[index + 1]
     }
+    serial.writeValue("Biggest Number", Biggestnumber)
 }
-serial.writeValue("Biggest Number", Biggestnumber)
 basic.forever(function () {
 	
 })
